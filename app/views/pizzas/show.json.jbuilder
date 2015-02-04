50b0e363b21d314b49c961b65916e7d72e86b3a9
Zach Cardais <zcardais@gmail.com>
2015-02-04 09:40:07 -0500

    Adds initial files and scaffolding

---
+++ app/views/pizzas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @pizza, :id, :name, :crust, :toppings, :cost, :image, :created_at, :updated_at
---
+++ db/migrate/20150204140123_create_pizzas.rb
@@ -0,0 +1,13 @@
+class CreatePizzas < ActiveRecord::Migration
+  def change
+    create_table :pizzas do |t|
+      t.string :name
+      t.string :crust
+      t.text :toppings
+      t.integer :cost
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
